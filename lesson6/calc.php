39385e7f576c7c0600bb862b38a72f68b69f400a
Alexanderkolt <aaaaa205@yandex.ru>
2021-12-26 00:53:53 +0300

    Now the numbers and operations are saved

--- lesson6/calc.php
+++ lesson6/calc.php
@@ -1,13 +1,16 @@
 <?php
 if(isset($_POST['x']))
     $x = (int)($_POST['x']);
-else $x=10;
+else 
+    $x=10;
 if(isset($_POST['y']))
     $y = (int)($_POST['y']);
-else $y=10;
-if(!isset($_POST['activity']))
+else 
+    $y=10;
+if(isset($_POST['activity']))
     $activity=htmlspecialchars($_POST['activity']);
-else $activity='+';
+else 
+    $activity='+';
 
 
 if(isset($x)&&isset($y))
@@ -35,14 +38,14 @@ if(isset($x)&&isset($y))
 
 <form action="" name="calc" method="POST">
     <p>Калькулятор</p>
-    <input type="number" name="x">
+    <input type="number" name="x" value="<?= $x;?>">
     <select name="activity" >
-        <option value="-">-</option>
-        <option value="+" selected>+</option>
-        <option value="/">/</option>
-        <option value="*">*</option>
+        <option <?php if ($activity=="-") echo "selected"?> value="-">-</option>
+        <option <?php if ($activity=="+") echo "selected"?> value="+">+</option>
+        <option <?php if ($activity=="/") echo "selected"?> value="/">/</option>
+        <option <?php if ($activity=="*") echo "selected"?> value="*">*</option>
     </select>
-    <input type="number" name="y">
+    <input type="number" name="y" value="<?= $y;?>">
     <input type="submit" name="=" value="=">
     <b><?php if (isset($z)) echo "$z"?></b>
     
